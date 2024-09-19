4830d665876db56c6703ff86032d74e50b842c27
Janiclecia Coelho <janicleciacoelho@gmail.com>
2024-09-18 21:34:46 -0300

    Add files via upload

---
+++ lista05.py
@@ -0,0 +1,22 @@
+def calcular_velocidade_dragao(distancia, dragao1, dragao2):
+
+    vel_dga1 = DISTANCIA / dragao1
+    vel_dga2 = DISTANCIA / dragao2
+
+
+    if   vel_dga1 > vel_dga2:
+         print('O Banguela (1) é o mais veloz!')
+    elif vel_dga2 > vel_dga1:
+         print('O Banguela não é o mais veloz!')
+    else:
+        print('Os dois dragões tem a mesma velocidade!')
+
+
+if __name__=='__main__':
+   DISTANCIA = 1000
+
+dragao1 = float(input('Digite o tempo do dragão 1:'))
+dragao2 = float(input('Digite o tempo do dragão 1:'))
+
+calcular_velocidade_dragao(DISTANCIA, dragao1, dragao2)
+
---
+++ lista06.q01.py
@@ -0,0 +1,13 @@
+def funcao_principal():
+    
+
+    for i in range(1000, 2001,1):
+
+
+
+
+        if i % 11 == 5:
+            print(f'Este valor{1} -> tem resto 5')
+
+if __name__ == '__main__':
+    funcao_principal()
